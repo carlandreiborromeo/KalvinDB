35dc118d10c3fe7254c49d199f03e0b04a1ab42f
carlandreiborromeo <164586077+carlandreiborromeo@users.noreply.github.com>
2025-01-22 02:14:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -39,12 +39,19 @@ app.post('/enroll', async (req, res) => {
   const { name, date_of_birth, course, email, phone_number } = req.body;
 
   try {
-    await sql.connect(config);
-    await sql.query
+    const pool = await sql.connect(config);
+    const query = `
       INSERT INTO enrollments (name, date_of_birth, course, email, phone_number)
-      VALUES (${name}, ${date_of_birth}, ${course}, ${email}, ${phone_number})
-    ;
+      VALUES (@name, @date_of_birth, @course, @name, @phone_number)
+    `;
+    const request = pool.request();
+    request.input('name', sql.NVarChar, name);
+    request.input('date_of_birth', sql.Date, date_of_birth);
+    request.input('course', sql.NVarChar, course);
+    request.input('email', sql.NVarChar, email);
+    request.input('phone_number', sql.NVarChar, phone_number);
 
+    await request.query(query);
     res.status(201).json({ msg: 'Enrollment data saved successfully' });
   } catch (err) {
     console.error('Error saving enrollment data:', err.message);
@@ -54,7 +61,5 @@ app.post('/enroll', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(Server is running on port ${port});
+  console.log(`Server is running on port ${port}`);
 });
-
-
